1d0ead24e9d1d3f8920f81b44fa0c88e798c5c3d
Ana Júlia <meloanajulia30@gmail.com>
2023-07-24 22:19:30 -0300

    piano
    javascript em andamento

--- js/app.js
+++ js/app.js
@@ -1,10 +1,34 @@
 const keys = document.querySelectorAll('.key');
 
+/* REPRODUÇÃO DAS NOTAS */
+const playNote = (note) => {
+    const audio = new Audio(`../notas/${note}.wav`);
+    audio.play();
+}
+
+/* click mouse */
 const handleMouseDown = (key) => { /* muda de estilo/background */
+    /* playNote(); */
+    key.getAttribute('data-note')
+    
+    if (key.className.includes('preta')) {
+        /* key.style.background = 'linear-gradient(#222, black)';
+        key.style.borderBottom = 'none';
+        key.style.height = '148px'; */
+        key.classList.add('preta-pressionada');
+        return;
+    }
+    
 key.style.background = '#ddd'; /* quando clicar no mouse */
 }
 
 const handleMouseUp = (key) => { /* muda de estilo/background */
+    
+    if (key.className.includes('preta')) {
+        key.classList.remove('preta-pressionada');
+    return;
+}
+    
 key.style.background = 'white'; /* quando soltar o mouse */
 }
 
