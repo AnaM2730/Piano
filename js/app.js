fb3ad66cbd33704f6420a636e4d8eda7b72e071a
Ana Júlia <meloanajulia30@gmail.com>
2023-07-24 22:37:24 -0300

    piano

--- js/app.js
+++ js/app.js
@@ -1,57 +1,46 @@
-const keys = document.querySelectorAll('.key');
+const keys = document.querySelectorAll(".key");
 
 /* REPRODUÇÃO DAS NOTAS */
 const playNote = (note) => {
-    const audio = new Audio(`../notas/${note}.wav`);
+    const audio = new Audio(`../note/${note}.wav`);
     audio.play();
-}
+};
 
 /* click mouse */
-const handleMouseDown = (key) => { /* muda de estilo/background */
-    playNote(key.getAttribute('data-note'));    
-    
-    if (key.className.includes('preta')) {
+const handleMouseDown = (key) => {
+    /* muda de estilo/background */
+    playNote(key.getAttribute("data-note"));
+
+    if (key.className.includes("preta")) {
         /* key.style.background = 'linear-gradient(#222, black)';
         key.style.borderBottom = 'none';
         key.style.height = '148px'; */
-        key.classList.add('preta-pressionada');
+        key.classList.add("preta-pressionada");
         return;
     }
-    
-key.style.background = '#ddd'; /* quando clicar no mouse */
-}
 
-const handleMouseUp = (key) => { /* muda de estilo/background */
-    
-    if (key.className.includes('preta')) {
-        key.classList.remove('preta-pressionada');
-    return;
-}
-    
-key.style.background = 'white'; /* quando soltar o mouse */
-}
+    key.style.background = "#ddd"; /* quando clicar no mouse */
+};
 
+const handleMouseUp = (key) => {
+    /* muda de estilo/background */
 
-keys.forEach((key) => { /* forEach - adiciona em cada elemento */
-    key.addEventListener('mousedown', () => handleMouseDown(key)) /* adiciona click */
-/* mousedown - quando clicar no mouse */
+    if (key.className.includes("preta")) {
+        key.classList.remove("preta-pressionada");
+        return;
+    }
 
-    key.addEventListener('mouseup', () => handleMouseUp(key)) /* adiciona click */
+    key.style.background = "white"; /* quando soltar o mouse */
+};
 
-}); 
+keys.forEach((key) => {
+    /* forEach - adiciona em cada elemento */
+    key.addEventListener("mousedown", () =>
+        handleMouseDown(key)
+    ); /* adiciona click */
+    /* mousedown - quando clicar no mouse */
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    key.addEventListener("mouseup", () =>
+        handleMouseUp(key)
+    ); /* adiciona click */
+});
